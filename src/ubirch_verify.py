21da379446ee0aa1840862cf3f90778bf634a009
Roxana Meixner <roxana.meixner@ubirch.com>
2022-09-04 13:26:21 +0200

    log verification service url and response in case of failed verification

--- src/ubirch_verify.py
+++ src/ubirch_verify.py
@@ -1,10 +1,20 @@
+import logging
+
 import requests as r
+
+logger = logging.getLogger()
 
 
 def verify(data_hash_base64: str, stage: str, bearer_token: str) -> bool:
-    url = f"https://verify.{stage}.ubirch.com"
+    url = f"https://verify.{stage}.ubirch.com/api/v2/upp/verify"
 
-    res = r.post(f"{url}/api/v2/upp/verify", data=data_hash_base64,
+    res = r.post(url,
+                 data=data_hash_base64,
                  headers={"content-type": "text/plain",
                           "Authorization": f"Bearer {bearer_token}"})
-    return res.status_code == 200
+
+    if res.status_code != 200:
+        logger.debug("verification at {} failed: {} {}".format(url, res.status_code, res.text))
+        return False
+
+    return True
